1deb31e8e4f886b35c59310cd54673aac4f11b2d
amantiwary12 <amantiwary2505@gmail.com>
2026-04-28 16:58:05 +0530

    new changes

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+   base: '/',
   server: {
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        // target: 'http://localhost:8000',
+        target: 'https://finance-expenses-tracker-backend.onrender.com',
         changeOrigin: true,
       },
     },
